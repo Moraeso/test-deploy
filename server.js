c9f125a464f7c87690b25d24ba5b9752d835a1af
Moraeso <sshn611@naver.com>
2020-12-04 18:24:54 +0900

    prod port 80 -> 443

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const ssrCache = new lruCache({
 });
 
 const dev = process.env.NODE_ENV !== 'production';
-const port = dev ? 3000 : 80;
+const port = dev ? 3000 : 443;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
