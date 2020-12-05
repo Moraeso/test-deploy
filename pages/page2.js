b1859245a5a9b6dccd88705856802a1ce0abf577
Moraeso <sshn611@naver.com>
2020-12-06 01:00:38 +0900

    delete Page2 getInitialProps

--- pages/page2.js
+++ pages/page2.js
@@ -1,22 +1,23 @@
 import Router from 'next/router';
 import { callApi } from '../src/api';
 
-Page2.getInitialProps = async ({ query }) => {
-  const { sayHello } = await import('../src/sayHello');
-  console.log(sayHello());
-  const text = query.text || 'none';
-  const data = await callApi();
-  return { text, data };
-};
+// Page2.getInitialProps = async ({ query }) => {
+//   const { sayHello } = await import('../src/sayHello');
+//   console.log(sayHello());
+//   const text = query.text || 'none';
+//   const data = await callApi();
+//   return { text, data };
+// };
 
-export default function Page2({ text, data }) {
+// export default function Page2({ text, data }) {
+export default function Page2() {
   return (
     <div>
       <button onClick={() => Router.push('/page1')}>page1로 이동</button>
       <p>기획팀: 블루프린트</p>
       <p>개발팀: 블루스크린</p>
-      <p>{`text: ${text}`}</p>
-      <p>{`data is ${data}`}</p>
+      <p>{`text: ...`}</p>
+      <p>{`data is ...`}</p>
     </div>
   )
 }
